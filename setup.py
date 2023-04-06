9f8dd820928855272fa17eab435fabffd9ed4d8e
Subho Halder <subho.halder@gmail.com>
2023-04-06 20:07:50 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+	description = fh.read()
+REQUIREMENTS = [i.strip() for i in open("api_key_detector/requirements.txt").readlines()]
+
+__VERSION__ = "0.0.1"
+
+setup(
+	name="ak-keydetector",
+	version=__VERSION__,
+	author="Appknox Engineering",
+	author_email="engineering@appknox.com",
+	packages=find_packages(),
+    include_package_data=True,
+	description="ML Based API Key Detector",
+	long_description=description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/appknox/ml-key-detector",
+	license='Apache-2.0 license',
+	python_requires='>=3.8',
+	install_requires=REQUIREMENTS
+)
